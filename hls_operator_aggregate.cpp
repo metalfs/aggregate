479bbc1f0c4b05e4a957128831eccac774b24363
Lukas Wenzel <lukas.wenzel@hpi.de>
2019-10-11 12:13:23 +0200

    Facilitate pipelining in row aggregate by using arrays explicitly

--- hls_operator_aggregate.cpp
+++ hls_operator_aggregate.cpp
@@ -16,6 +16,37 @@ typedef ap_uint<VALUE_BITS*VALUE_COUNT> Row;
 typedef ap_uint<VALUE_BITS*VALUE_COUNT*2> DoubleRow;
 typedef ap_uint<VALUE_COUNT> Mask;
 
+typedef ap_uint<VALUE_BITS*4> Stats;
+
+Stats aggregate_row(Row row) {
+  Stats stats;
+  Value local_cnt[VALUE_COUNT+1];
+  Value local_sum[VALUE_COUNT+1];
+  Value local_min[VALUE_COUNT+1];
+  Value local_max[VALUE_COUNT+1];
+  local_cnt[0] = 0;
+  local_sum[0] = 0;
+  local_min[0] = 0xffffffffffffffffull;
+  local_max[0] = 0;
+
+#pragma HLS PIPELINE
+  for (int i = 0; i < VALUE_COUNT; ++i) {
+#pragma HLS UNROLL
+    Value value = row(VUP(i), VLO(i));
+    local_cnt[i+1] = local_cnt[i] + 1;
+    local_sum[i+1] = local_sum[i] + value;
+    local_min[i+1] = (value < local_min[i]) ? value : local_min[i];
+    local_max[i+1] = (value > local_max[i]) ? value : local_max[i];
+  }
+
+  stats(VUP(0), VLO(0)) = local_cnt[VALUE_COUNT];
+  stats(VUP(1), VLO(1)) = local_sum[VALUE_COUNT];
+  stats(VUP(2), VLO(2)) = local_min[VALUE_COUNT];
+  stats(VUP(3), VLO(3)) = local_max[VALUE_COUNT];
+  return stats;
+}
+
+
 void hls_operator_aggregate(mtl_stream &in, mtl_stream &out) {
 #pragma HLS INTERFACE axis port=in name=axis_input
 #pragma HLS INTERFACE axis port=out name=axis_output
@@ -32,18 +63,11 @@ void hls_operator_aggregate(mtl_stream &in, mtl_stream &out) {
 #pragma HLS PIPELINE
     input = in.read();
 
-    for (int i = 0; i < VALUE_COUNT; ++i) {
-#pragma HLS UNROLL
-      Value value = input.data(VUP(i), VLO(i));
-      count += 1;
-      sum += value;
-      if (value < min) {
-        min = value;
-      }
-      if (value > max) {
-        max = value;
-      }
-    }
+    Stats stats = aggregate_row(input.data);
+    count += stats(VUP(0), VLO(0));
+    sum += stats(VUP(1), VLO(1));
+    min = (stats(VUP(2), VLO(2)) < min) ? stats(VUP(2), VLO(2)) : min;
+    max = (stats(VUP(3), VLO(3)) > max) ? stats(VUP(3), VLO(3)) : max;
 
   } while (!input.last);
 
